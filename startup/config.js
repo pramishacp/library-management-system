125d12c41d2096b1538d2d64bec856a2755c7b4c
pramishacp <pramishacp123@gmail.com>
2022-03-04 18:57:50 +0530

    should return test when NODE_ENV=test

--- startup/config.js
+++ startup/config.js
@@ -8,7 +8,13 @@ const config = {
             port: parseInt(process.env.DEV_APP_PORT),
             env: 'dev'
         }
-    }
+    },
+    'test': {
+        app: {
+            port: parseInt(process.env.TEST_APP_PORT),
+            env: 'test'
+        }
+    },
 }
 
 module.exports = config[env];
