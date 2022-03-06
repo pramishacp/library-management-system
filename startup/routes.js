3cb308c79acdf101e8fc405f33e89b20ef96a424
pramishacp <pramishacp123@gmail.com>
2022-03-06 16:56:13 +0530

    add morgan and cors packages

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan')
 
 const { borrows, books, users } = require('../lib/index')
 
 module.exports = function(app) {
     app.use(express.json());
+    app.use(cors())
+    app.use(morgan('tiny'))
     app.use('/api/users', users);
     app.use('/api/books', books);
     app.use('/api/borrows', borrows);
